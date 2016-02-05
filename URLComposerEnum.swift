d5310bf873eab88312726f23f506a752781452f2
Lukasz <lukasz@Lukaszs-MBP-2.chello.pl>
2016-02-05 09:39:47 +0100

    add enum to SwiftLab

---
+++ URLComposerEnum.swift
@@ -0,0 +1,32 @@
+enum URLComposer {
+  case UserCreate
+  case UserUpdate
+  case UserRemove
+  case UserShow
+
+  var method: String {
+    switch self {
+      case .UserCreate:
+        return "POST"
+      case .UserUpdate:
+        return "POST"
+      case .UserRemove:
+        return "DELETE"
+      case .UserShow
+        return "GET"
+    }
+  }
+
+  var path: String {
+    switch self {
+      case .UserCreate:
+        return "/users/create"
+      case .UserUpdate:
+        return "/user/1/update"
+      case .UserRemove:
+        return "/user/1/delete"
+      case .UserShow
+        return "/user/1/""
+    }
+  }
+}
